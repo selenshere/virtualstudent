6563a92422e282ba70a8edda2dcaf9b45736a023
Selen <110853498+selenshere@users.noreply.github.com>
2025-12-12 04:14:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BACKEND_URL="https://YOUR-RENDER.onrender.com";
+const BACKEND_URL="https://virtualstudent.onrender.com";
 let conversation=[];
 async function callBackend(conv){
  const r=await fetch(BACKEND_URL+"/chat",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({conversation:conv})});
